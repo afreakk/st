61f1d35ff09a4736e7f1edb3d03790f50c30f1a7
hans <laderud@hotmail.com>
2020-05-30 17:16:28 +0200

    remove debug poop

--- scripts/autocomplete.sh
+++ scripts/autocomplete.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 word=$(tr ' ' '\n' | tac | sed 's/^ *//;s/ *$//' | awk '!cnts[$0]++' | dmenu | tr -d '\n')		    
-echo $@ > ~/wot
 
 if [ "$1" == "clipboard" ]; then
     echo -n $word | xclip -selection clipboard
